abf92485847fa607ab6fded6ee7773f497a1f58f
hassan <hassanpudineh67@gmail.com>
2022-11-26 17:34:51 +0330

    add event listener on number check click

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
 'use strict';
-
+/*
 console.log(document.querySelector(".message").textContent);
 document.querySelector('.message').textContent = '😊  Correct Number...!'
 
 document.querySelector('.score').textContent = 20;
 
 document.querySelector('.guess').value = 12;
+*/
+
+document.querySelector('.check').addEventListener('click', function(){
+      const guess = Number(document.querySelector('.guess').value);
+      if(!guess) {
+            document.querySelector('.message').textContent = '⛔ No number!'
+      }
+})
